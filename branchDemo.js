f1a2aa6682fd00e5e2f4ea8df8e88fd4ae6b3733
kzz9984 <92399816+kzz9984@users.noreply.github.com>
2023-09-11 12:30:48 -0400

    Update branchDemo.js
    1. Added day and year info to branchDemo.

--- branchDemo.js
+++ branchDemo.js
@@ -9,6 +9,14 @@ const months = ["January", "February", "March", "April",
 const d = new Date();
 
 // getMonth() returns the month as an integer (0-11). Jan = 0, Dec = 11
-let month = months[d.getMonth()]
+// getDay() returns the day as an integer (1-31)
+// getFullYear() returns the year as a 4 digit number
 
-console.log("The month is: " + month)
+let month = months[d.getMonth()]
+let day = d.getDay()
+let year = d.getFullYear()
+
+let currentDate = "The current date is: " + month + " " + day + ", " + year
+
+console.log(d)
+console.log(currentDate)
