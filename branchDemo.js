88eaee4763c05a175513fa9dc47d07b5b0a5529a
kzz9984 <92399816+kzz9984@users.noreply.github.com>
2023-09-11 12:45:09 -0400

    Update branchDemo.js

--- branchDemo.js
+++ branchDemo.js
@@ -9,11 +9,11 @@ const months = ["January", "February", "March", "April",
 const d = new Date();
 
 // getMonth() returns the month as an integer (0-11). Jan = 0, Dec = 11
-// getDay() returns the day as an integer (1-31)
+// getDate() returns the day as an integer (1-31)
 // getFullYear() returns the year as a 4 digit number
 
 let month = months[d.getMonth()]
-let day = d.getDay()
+let day = d.getDate()
 let year = d.getFullYear()
 
 let currentDate = "The current date is: " + month + " " + day + ", " + year
