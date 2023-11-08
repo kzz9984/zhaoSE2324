79cf6df44cc74ada13212bce51e7eb688ab70381
kzz9984 <92399816+kzz9984@users.noreply.github.com>
2023-11-08 11:06:37 -0500

    added chart to results

--- JavaScript-Lessons/chartJS-examples/js/lineChart.js
+++ JavaScript-Lessons/chartJS-examples/js/lineChart.js
@@ -5,7 +5,7 @@
 async function getData() {
     const response = await fetch('../data/global-mean-temp.csv');
     const data = await response.text();     // CSV is in TEXT format
-    console.log(data);
+    //console.log(data);
 
     const xYears = [];      // x-axis labels = years values
     const yTemps = [];      // y-axis global temp values
@@ -17,7 +17,7 @@ async function getData() {
     // slice(start, end) - return a new array starting at index start
     //                     up to but not including index end.
     const table = data.split('\n').slice(1);
-    console.log(table);
+    //console.log(table);
 
     table.forEach(row => {
         const columns = row.split(',');         // split each row on the commas
@@ -33,8 +33,102 @@ async function getData() {
         const shTemp = parseFloat(columns[3]);  // s. hemi. temp deviation values
         ySHtemps.push(shTemp + 14);
 
-        console.log(year, temp, nhTemp, shTemp);
-    })
+        //console.log(year, temp, nhTemp, shTemp);
+    });
+    return {xYears, yTemps, yNHtemps, ySHtemps};
 }
 
-getData();
+async function createChart() {
+    const data = await getData();    // createChart will wait until getData() is finished processing
+    const ctx = document.getElementById('myChart');
+    const degSym = String.fromCharCode(176);
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: data.xYears,
+            datasets: [
+                {
+                    label: `Combined Global Land-Surface Air and Sea-Surface Water Temperature in ${degSym}C`,
+                    data: data.yTemps,
+                    fill: false,
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    borderWidth: 1
+                },
+                {
+                    label: `Combined N.H. Land-Surface Air and Sea-Surface Water Temperature in ${degSym}C`,
+                    data: data.yNHtemps,
+                    fill: false,
+                    backgroundColor: 'rgba(0, 102, 255, 0.2)',
+                    borderColor: 'rgba(0, 102, 255, 1)',
+                    borderWidth: 1
+                },
+                {
+                    label: `Combined S.H. Land-Surface Air and Sea-Surface Water Temperature in ${degSym}C`,
+                    data: data.ySHtemps,
+                    fill: false,
+                    backgroundColor: 'rgba(0, 153, 51, 0.2)',
+                    borderColor: 'rgba(0, 153, 51, 1)',
+                    borderWidth: 1
+                }
+            ]
+        },
+        options: {
+            responsive: true,   // Re-size based on screen size
+            scales: {           // Display options for x & y axes
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Year',   // x-axis title
+                        font: {         // font properties
+                            size: 20
+                        }
+                    },
+                    ticks: {
+                        callback: function(val, index) {
+                            // Labeling of tick marks can be controlled by code and font size
+                            return index % 5 === 0 ? this.getLabelForValue(val) : '';
+                        },
+                        font: {
+                            size: 16
+                        }
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: 'Global Mean Temperatures (°C)',
+                        font: {
+                            size: 20
+                        }
+                    },
+                    ticks: {
+                        maxTicksLimit: data.yTemps.length/10,    // limit # of ticks
+                        font: {
+                            size: 12
+                        }
+                    }
+                }
+            },
+            plugins: {          // Display options
+                title: {
+                    display: true,
+                    text: 'Global Mean Temperature vs. Year (since 1880)',
+                    font: {
+                        size: 24
+                    },
+                    padding: {
+                        top: 10,
+                        bottom: 30
+                    }
+                },
+                legend: {
+                    align: 'start',
+                    position: 'bottom'
+                },
+            }
+        }
+    });
+}
+
+createChart();
